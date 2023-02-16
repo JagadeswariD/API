dd82feb4f94dffc3da172283cb897395f43fe045
Joyce Tan <joycetan1298@gmail.com>
2023-02-16 12:22:09 -0500

    Added title form

--- ExternalApiProject/src/main/java/com/externalapi/controller/MovieController.java
+++ ExternalApiProject/src/main/java/com/externalapi/controller/MovieController.java
@@ -1,7 +1,12 @@
 package com.externalapi.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.externalapi.model.Movie;
 
 @Controller
 public class MovieController {
@@ -10,5 +15,20 @@ public class MovieController {
 	String hello() {
 		return "index";
 	}
+	
+	@GetMapping("/searchTitle")
+    public String loginForm(Model model) {
+        model.addAttribute("myMovie", new Movie());
+        return "titleForm";
+    }
+	
+    @PostMapping("/saveTitle")
+    public String saveLoginSubmission(Model model, Movie myMovie) {
+    	
+    	System.out.println(">>>>>> title=" + myMovie.getTitle());
+    	model.addAttribute("myMovie", myMovie);
+        return "index";
+    }
+
 
 }
